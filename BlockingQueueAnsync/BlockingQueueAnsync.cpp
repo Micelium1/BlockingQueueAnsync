21536145918b38997ae2f622461daac0e5572dde
Micelium <maks.237@mail.ru>
2024-10-23 15:30:22 +0300

    Улучшеное тестирование

--- BlockingQueueAnsync/BlockingQueueAnsync.cpp
+++ BlockingQueueAnsync/BlockingQueueAnsync.cpp
@@ -3,38 +3,47 @@
 #include <future>
 #include <vector>
 #include <chrono>
+
+std::atomic<int> enqueCounter = 0;
+std::atomic<int> dequeCounter = 0;
+
 void addNumbers(BlockingQueue<int>* queue)
 {
     int pocket = 100;
     for (int i = 0; i < pocket; ++i)
     {
         queue->atomicEnque(i);
+        ++enqueCounter;
         std::this_thread::sleep_for(std::chrono::microseconds(rand()));
     }
 }
 void unNumber(BlockingQueue<int>* queue)
 {
-    while (true)
+    while (!queue->isEmpty())
     {
         queue->atomicDeque();
+        ++dequeCounter;
         std::this_thread::sleep_for(std::chrono::microseconds(rand()));
     }
 }
 int main()
 {
-    srand(time(NULL));
-    BlockingQueue<int> queue(5);
-    std::vector<std::future<void>> inFuncs;
-    std::vector<std::future<void>> outFuncs;
-    for (int i = 0; i < 8; ++i)
     {
-        inFuncs.push_back(std::async(std::launch::async, &addNumbers,&queue));
-        
-    }
-    for (int i = 0; i < 4; ++i)
-    {
-        outFuncs.push_back(std::async(std::launch::async, &unNumber, &queue));
+        srand(time(NULL));
+        BlockingQueue<int> queue(5);
+        std::vector<std::future<void>> inFuncs;
+        std::vector<std::future<void>> outFuncs;
+        for (int i = 0; i < 8; ++i)
+        {
+            inFuncs.push_back(std::async(std::launch::async, &addNumbers, &queue));
 
+        }
+        for (int i = 0; i < 2; ++i)
+        {
+            outFuncs.push_back(std::async(std::launch::async, &unNumber, &queue));
+
+        }
     }
+    std::cout << enqueCounter << " " << dequeCounter;
 }
 
